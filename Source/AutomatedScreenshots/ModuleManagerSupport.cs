d3020e7e633d90e9d594a835212b860eed1a2727
Lisias T <me@lisias.net>
2021-11-04 21:03:09 -0300

    Moving the thingy into `net.lisias.ksp` vendor hierarchy

---
+++ Source/AutomatedScreenshots/ModuleManagerSupport.cs
@@ -0,0 +1,30 @@
+﻿/*
+	This file is part of Automated Screenshots /L Unleashed
+		© 2018-2021 Lisias T : http://lisias.net <support@lisias.net>
+		© 2015-2018 LinuxGuruJamer
+
+	Automated Screenshots /L Unleashed is licensed as follows:
+		* GPL 3.0 : https://www.gnu.org/licenses/gpl-3.0.txt
+
+	Automated Screenshots /L Unleashed is distributed in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+	warranty of	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	You should have received a copy of the GNU General Public License 3.0
+	along with Automated Screenshots /L Unleashed.
+	If not, see <https://www.gnu.org/licenses/>.
+
+*/
+using System.Collections.Generic;
+
+namespace AutomatedScreenshots
+{
+	public static class ModuleManagerSupport
+	{
+		public static IEnumerable<string> ModuleManagerAddToModList()
+		{
+			string[] r = { typeof(ModuleManagerSupport).Namespace };
+			return r;
+		}
+	}
+}
